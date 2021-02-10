5cc198fee269fe3bb60d78f6ccf2ab8b56434350
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-02-10 21:06:42 +0700

    Create Snooping for suspects.py

---
+++ 1.Getting Started in Python/Snooping for suspects.py
@@ -0,0 +1,9 @@
+# Define plate to represent a plate beginning with FRQ
+# Use * to represent the missing four letters
+plate = 'FRQ****'
+
+# Call the function lookup_plate()
+lookup_plate(plate)
+
+# Call lookup_plate() with the keyword argument for color
+lookup_plate(plate, color='Green')
