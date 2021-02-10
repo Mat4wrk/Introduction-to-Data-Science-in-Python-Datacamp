39b1b5e4548c6be8f5a06e3f982c701a4c7f6230
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-02-10 21:11:06 +0700

    Create Two methods for selecting columns.py

---
+++ 2.Loading Data in pandas/Two methods for selecting columns.py
@@ -0,0 +1,14 @@
+# Select the column item from credit_records
+# Use brackets and string notation
+items = credit_records.item
+
+# Display the results
+print(items)
+
+
+# Select the column item from credit_records
+# Use dot notation
+items = credit_records.item
+
+# Display the results
+print(items)
