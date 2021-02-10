a65876fcb90bd6f7e1f7c4519e875f728c35a3a7
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-02-10 21:15:28 +0700

    Create Working hard.py

---
+++ 3.Plotting Data with matplotlib/Working hard.py
@@ -0,0 +1,8 @@
+# From matplotlib, import pyplot under the alias plt
+from matplotlib import pyplot as plt
+
+# Plot Officer Deshaun's hours_worked vs. day_of_week
+plt.plot(deshaun.day_of_week, deshaun.hours_worked)
+
+# Display Deshaun's plot
+plt.show(
