8138e458719c93b83a41647b50c5a677f7fdcf73
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-02-10 21:12:10 +0700

    Create More column selection mistakes.py

---
+++ 2.Loading Data in pandas/More column selection mistakes.py
@@ -0,0 +1,15 @@
+# Use info() to inspect mpr
+print(mpr.info())
+
+# The following code contains one or more errors
+# Correct the mistakes in the code so that it runs without errors
+
+# Select column "Dog Name" from mpr
+name = mpr['Dog Name']
+
+# Select column "Missing?" from mpr
+is_missing = mpr['Missing?']
+
+# Display the columns
+print(name)
+print(is_missing)
