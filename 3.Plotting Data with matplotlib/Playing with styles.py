2e24a71102b8b28c15969ec0919b46ef75d172ab
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-02-10 21:20:50 +0700

    Create Playing with styles.py

---
+++ 3.Plotting Data with matplotlib/Playing with styles.py
@@ -0,0 +1,47 @@
+#Change the plotting style to "fivethirtyeight".
+# Change the style to fivethirtyeight
+plt.style.use('fivethirtyeight')
+
+# Plot lines
+plt.plot(data["Year"], data["Phoenix Police Dept"], label="Phoenix")
+plt.plot(data["Year"], data["Los Angeles Police Dept"], label="Los Angeles")
+plt.plot(data["Year"], data["Philadelphia Police Dept"], label="Philadelphia")
+
+# Add a legend
+plt.legend()
+
+# Display the plot
+plt.show()
+
+
+#Change the plotting style to "ggplot".
+# Change the style to ggplot
+plt.style.use('ggplot')
+
+# Plot lines
+plt.plot(data["Year"], data["Phoenix Police Dept"], label="Phoenix")
+plt.plot(data["Year"], data["Los Angeles Police Dept"], label="Los Angeles")
+plt.plot(data["Year"], data["Philadelphia Police Dept"], label="Philadelphia")
+
+# Add a legend
+plt.legend()
+
+# Display the plot
+plt.show()
+
+
+#View all styles by typing print(plt.style.available) in the console
+#Pick one of those styles and see what it looks like
+# Choose any of the styles
+plt.style.use('seaborn-talk')
+
+# Plot lines
+plt.plot(data["Year"], data["Phoenix Police Dept"], label="Phoenix")
+plt.plot(data["Year"], data["Los Angeles Police Dept"], label="Los Angeles")
+plt.plot(data["Year"], data["Philadelphia Police Dept"], label="Philadelphia")
+
+# Add a legend
+plt.legend()
+
+# Display the plot
+plt.show()
