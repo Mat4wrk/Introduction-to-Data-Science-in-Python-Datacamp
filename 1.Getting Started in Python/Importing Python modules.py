08d220bf61a74627faf67b79c27531b306d05ba2
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-02-10 21:01:36 +0700

    Create Importing Python modules.py

---
+++ 1.Getting Started in Python/Importing Python modules.py
@@ -0,0 +1,8 @@
+# Use an import statement to import statsmodels
+import statsmodels
+
+# Import statsmodels under the alias sm
+import statsmodels as sm
+
+# Use an import statement to import seaborn with alias sns
+import seaborn as sns
